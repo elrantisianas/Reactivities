917d70c16e9562560ba72a495880e0668b7bc52d
Anas Rantisi <arantisi@Beyesolutions.local>
2020-07-18 11:27:45 +0300

    Initial Commit

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Presistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var servicies = scope.ServiceProvider;
+                try
+                {
+                    var context = servicies.GetRequiredService<DataContext>();
+
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+
+                    var logger = servicies.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An Error occured during migration");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
